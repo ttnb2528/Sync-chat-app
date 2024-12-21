460cf6915845ae9c548856049585ef28004df650
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-12-21 15:39:13 +0700

    update v0.29

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.use("/api/v1/channel", channelRoutes);
 const __dirname1 = path.resolve("");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../", "client", "dist")));
+  app.use(express.static(path.join(__dirname1, "./dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "dist", "index.html"));
