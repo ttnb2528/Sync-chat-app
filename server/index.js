34b368fc7aee8b7bb3bae422b2034e0d0949a9de
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-12-21 15:03:41 +0700

    upodate v0.24

--- server/index.js
+++ server/index.js
@@ -41,9 +41,11 @@ const __dirname1 = path.resolve("");
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "../", "client", "dist")));
-  
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "dist", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname1, "../", "client", "dist", "index.html")
+    );
   });
 } else {
   app.get("/", (req, res) => {
