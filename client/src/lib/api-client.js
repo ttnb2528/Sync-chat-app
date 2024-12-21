d0bc0f42332e44bfd89920e4aa68868c460f2413
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-12-21 14:27:04 +0700

    update 0.20

--- client/src/lib/api-client.js
+++ client/src/lib/api-client.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import { HOST } from "@/utils/constants.js";
 
 export const apiClient = axios.create({
-  baseURL: HOST,
+  baseURL: import.meta.env.NODE_ENV === "development" ? HOST : "https://sync-chat-app.onrender.com/",
   withCredentials: true,
 });
